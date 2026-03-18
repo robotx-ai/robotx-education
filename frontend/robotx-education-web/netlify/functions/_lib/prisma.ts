19b2a0851b71353f0c50ae5105ed18665eab5afa
Carrie Wang <carrie@borderxai.com>
2026-03-18 16:27:01 -0700

    fix: prisma fix for production

--- frontend/robotx-education-web/netlify/functions/_lib/prisma.ts
+++ frontend/robotx-education-web/netlify/functions/_lib/prisma.ts
@@ -3,9 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error", "warn"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log: ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") {
